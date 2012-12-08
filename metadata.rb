b4794df9dc4aab5fe7d86f4fc996e1adba74e66f
Lucas Jandrew <fapiko@gmail.com>
2012-12-08 15:21:34 -0800

    Exporting java's LD_LIBRARY_PATH setting now to cope with some display issues on the Linux

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          "All rights reserved"
 description      "Installs/Configures minecraft"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+
+depends 'java'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'java'
+
 case node[:minecraft][:deployment]
   when 'both'
     include_recipe "#{@cookbook_name}::application"
