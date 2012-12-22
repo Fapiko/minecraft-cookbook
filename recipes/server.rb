61733198eefa3c580828b1996f8eb0d32ebb9897
Lucas Jandrew <fapiko@gmail.com>
2012-12-22 03:41:33 -0800

    Moved craftbukkit jar into an attribute

--- recipes/server.rb
+++ recipes/server.rb
@@ -38,7 +38,7 @@ remote_file "#{node[:minecraft][:base_dir]}/server/minecraft_server.jar" do
 end
 
 remote_file "#{node[:minecraft][:base_dir]}/server/craftbukkit-beta.jar" do
-  source 'https://dl.dropbox.com/u/50768347/craftbukkit-1.4.5-R1.0.jar'
+  source node[:minecraft][:craftbukkit][:server_jar]
   mode 0644
   user 'minecraft'
   group 'minecraft'
