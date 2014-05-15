5065613bce04b033ff41c10b772665d3bfce3792
Lucas Jandrew <lucas@jandrew.com>
2014-05-15 00:52:35 -0700

    Switching license to MIT

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "minecraft"
 maintainer       "Lucas Jandrew"
 maintainer_email "lucas@jandrew.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures minecraft"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
